16e67c8c67773ba66c02e93a8003b0900f4edb98
L05TFuture <keithsu6705@gmail.com>
2024-05-13 21:32:53 +0800

    finished the logic function for rock paper scissors on playRound

--- script.js
+++ script.js
@@ -8,15 +8,35 @@ function getComputerChoice()
     return x
 }
 
-let c= getComputerChoice()
 
-console.log(c)
 
 function getHumanChoice()
 {let p=prompt("choose rock paper or scissors")
-if (p!="rock"||"paper"||"scissors"){return 'error'}
-return p
+//if (p!="rock"&&p!="paper"&&p!="scissors"){return 'error'}
+return p.toLowerCase()
 
 }
-let h=getHumanChoice()
-console.log(h)
+
+
+let humanScore=0
+let computerScore=0
+let scoreboard= humanScore + ':' + computerScore
+
+function playRound(humanChoice,computerChoice)
+{
+  if (humanChoice=='rock' && computerChoice=='rock'){console.log("Its a tie!")}
+ else if(humanChoice=='rock' && computerChoice=='scissors'){console.log('You win! rock beats scissors'); ++humanScore; }
+ else if (humanChoice=='rock' &&  computerChoice=='paper'){console.log('You lose! rock loses to paper'); ++computerScore; }
+ else if (humanChoice=='paper' && computerChoice=='paper'){console.log('Its a tie!')}
+ else if (humanChoice=='paper' && computerChoice=='scissors'){console.log('You lose! paper loses to rock'); ++computerScore;}
+ else if (humanChoice=='paper' && computerChoice=='rock'){console.log('You win! paper beats rock');humanScore++}
+ else if (humanChoice=='scissors' && computerChoice=='scissors'){console.log('Its a tie!')}
+ else if (humanChoice=='scissors' && computerChoice=='rock'){console.log('You lose! scissors loses to rock'); computerScore++}
+ else if (humanChoice=='scissors' && computerChoice=='paper'){console.log('You win! scissors beats paper'); computerScore++}
+ scoreboard = humanScore + ':' + computerScore;
+ console.log(scoreboard);
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection)
